390f8257ef31e4c794e32c7c27376569d0a109d7
Hoang Phuc <hvphuc081299@gmail.com>
2018-11-18 21:29:23 +0700

    Move Scenes

---
+++ Scripts/LoadLevel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+	public void PlayAgain() {
+		Application.LoadLevel ("GamePlay");
+	}
+}
