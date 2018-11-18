c08ed09bcca0d359209055eb4f0cc91808f252b4
Hoang Phuc <hvphuc081299@gmail.com>
2018-11-18 21:27:28 +0700

    Creata button and add event

---
+++ Scripts/AddButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour {
+	[SerializeField] private Transform panel;
+	[SerializeField] private GameObject Button;
+	GameObject btn;
+	//Ham tao cac button
+	void Awake() {
+		for (int i = 0; i < 20; i++) {
+			btn = Instantiate (Button);
+			btn.name = "" + i;
+			btn.transform.SetParent (panel, false);
+		}
+	}
+}
